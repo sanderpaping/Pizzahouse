0a2cfb66a4ef599bd6fbb96b6afc12ca8b64dd99
Sander Paping <sander.paping@student.stenden.com>
2020-03-30 21:54:55 +0200

    Course complete

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -7,6 +7,10 @@ use App\Pizza;
 
 class PizzaController extends Controller
 {
+    // public function __construct(){
+    //     $this->middleware('auth');
+    // }
+    
     public function index() {
         
     // $pizzas = Pizza:: all();
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
             The north's best pizzaplace
         </div>
         <p class="mssg">{{ session('mssg')}}</p>
-        <a href="/pizzas/create">Order a pizza</a>
+        <a href="{{ route('pizzas.create') }}">Order a pizza</a>
     </div>
 </div>
 @endsection
